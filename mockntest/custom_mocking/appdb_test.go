6df34c13ac54f35d34e2056a80b41fcf5a74ec1a
linonymous <iamlinonymous@gmail.com>
2020-01-17 18:14:06 +0530

    - add appdb_test

--- mockntest/custom_mocking/appdb_test.go
+++ mockntest/custom_mocking/appdb_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/linonymous/GoLearn/mockntest/custom_mocking/db"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type MockDB struct {
+	db.DML
+}
+
+func (m *MockDB) ListTable() []string  {
+	return []string{"abc", "mno", "pqr"}
+}
+func TestGetTables(t *testing.T) {
+	d := &MockDB{}
+	a := GetTables(d)
+	assert.Equal(t, a[0], "abc")
+	assert.Equal(t, a[1], "mno")
+	assert.Equal(t, a[2], "pqr")
+}
